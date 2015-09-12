c82ec18b86e7718ef42fb6cb4b35206df075f2ca
Mac <mac@Macs-MacBook.local>
2015-09-12 08:47:01 +0530

    Initial Commit

---
+++ UITableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableView
+//
+//  Created by Mac on 12/09/15.
+//  Copyright (c) 2015 Peers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableView
+//
+//  Created by Mac on 12/09/15.
+//  Copyright (c) 2015 Peers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
